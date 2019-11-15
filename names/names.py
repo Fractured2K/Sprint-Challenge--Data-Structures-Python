5eaa8857ac869fa6cc0072ffd3d5e767a697bcdc
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-15 09:22:09 -0800

    Complete names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
 import time
+import sys
+sys.path.append('../datastructures')
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,12 +14,16 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+bst = BinarySearchTree("Skyelar Carroll")
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
