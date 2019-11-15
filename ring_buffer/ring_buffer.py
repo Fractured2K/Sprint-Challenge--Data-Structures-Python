f4d9f6ac364580fc91dc59b02ac2c8f700da2acd
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-11-15 08:51:10 -0800

    Complete ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,32 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        # Check buffer capacity, if at capacity, reset self current
+        if self.current is self.capacity:
+            self.current = 0
 
-  def get(self):
-    pass
+        # Increment current amount of items in buffer
+        self.current += 1
+        # Replace item in current buffer position
+        self.storage[self.current - 1] = item
+
+    def get(self):
+        items = []
+
+        for i in self.storage:
+            if i is not None:
+                items.append(i)
+
+        return items
+
+
+buffer2 = RingBuffer(5)
+for i in range(50):
+    buffer2.append(i)
+    print(buffer2.get())
+
+print("last", buffer2.get())
